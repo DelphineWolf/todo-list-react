ab10ff03f5e979d82e1917de6400880de301c8fa
Delphine Wolf <bwb.wolf.d@fondespierre.com>
2026-02-04 12:45:26 +0100

    ajout de la fonction Ajouter tâche

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,33 @@ import './App.css'
 
 
 function App() {
+  //Tableau des tâches
 const [taches,setTaches]=useState([
 {id:1,text:'Apprendre React',completed: false},
 {id:2,text:'Apprendre Flutter',completed: false},
 ]);
 
+//Input d'ajout de tâche
+const [inputValeur,setInputValeur]=useState('');
+
+//-------------------Fonction Ajouter tâche------------
+function ajouterTache(){
+  if (inputValeur !==''){
+    const nouvelleTache ={id: Date.now(),text:inputValeur,completed:false}
+    setTaches([...taches, nouvelleTache])
+    setInputValeur('')
+  }
+}
   return (
 
 <div className='app'>
 
 <h1>Todo List</h1>
 <div className='tache-input'>
-  <input type="text" placeholder="Ajouter une tâche..." />
-  <button>Ajouter</button>
+  <input type="text" placeholder="Ajouter une tâche..." value={inputValeur} onChange={(e)=> setInputValeur(e.target.value)}/>
+  <button onClick={ajouterTache}>Ajouter</button>
 </div>
 
-{/*Création de l'enfant liste en dur */}
 
 <ul className='taches-liste'>
  {taches.map((tache) => (
